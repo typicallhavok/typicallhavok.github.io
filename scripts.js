bef296ab637e15cc1e2fc1db9b05fdde05f41065
typicallhavok <diamondpineappleboy@gmail.com>
2023-08-26 03:00:30 +0530

    almost

--- scripts.js
+++ scripts.js
@@ -1,13 +1,40 @@
 let index=0;
 let typLi = ['/graphic-design','/freelance','/full-stack']
-const aboutLink = document.querySelector('.abouz a');
-const aboutDiv = document.querySelector('#about');
+const aboutDiv = document.querySelector('#About');
 const typm = document.querySelectorAll('.tj');
 const aText = aboutDiv.querySelectorAll('.info');
 const buttons = document.querySelectorAll('.button-wrapper');
+const imgs = document.querySelectorAll('img');
+const projDiv = document.querySelector('#Project');
 let homeLoad;
+let currPage='home';
+let tit = document.title.toUpperCase();
+let x=0;
+let i=0;
+let l=tit.length;
 
-async function home(){
+function closeAll() {
+    aboutDiv.style.display='none' ;
+    clearInterval(homeLoad);
+    typm.forEach(element => {
+        element.style.display = 'none';
+    });
+    aText.forEach(element => {
+        typeText(element,element.textContent);
+    });
+    projDiv.style.display = 'none';
+}
+
+function openHome() {
+    typm.forEach(element => {
+        element.style.display = 'flex';
+    });
+    setTimeout(() => {
+        homeLoad = setInterval(home, 2000);
+    }, 1000);
+}
+
+function home(){
     const typ = document.getElementById("typ");
     typ.textContent = typLi[index];
     if(index==typLi.length-1)
@@ -25,39 +52,60 @@ function typeText(element, text) {
       }
     }
     animateTyping();
-  }
+}
 
-aboutLink.addEventListener('click', (event) => {
-    event.preventDefault();
-    if(aboutDiv.style.display == 'none' || aboutDiv.style.display == '') {
-        charIndex =0;
-        aboutDiv.style.display = 'block';
-        clearInterval(homeLoad);
-        console.log('timeoutpause')
-        typm.forEach(element => {
-            element.style.display = 'none';
-        });
-        aText.forEach(element => {
-            typeText(element,element.textContent);
-        });
+function titlee(){
+    if(i==l)
+    x=1;
+    else if(i==1)
+    x=0;
+    if(x==0){
+        i++;
+        document.title=tit.substring(0,i)
     }
     else {
-        aboutDiv.style.display='none' ;
-        typm.forEach(element => {
-            element.style.display = 'flex';
-        });
-        setTimeout(() => {
-            homeLoad = setInterval(home, 2000);
-        }, 1000);
+        i--;
+        document.title=tit.substring(0,i)
     }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const navLinks = document.querySelectorAll('nav a');
+    navLinks.forEach(link => {
+        link.addEventListener('click', function(e) {
+            const targetId = this.getAttribute('href');
+            if(targetId=='#About'||targetId=='#Project') {
+                e.preventDefault();
+                closeAll();
+            }
+            if(targetId==currPage) {
+                closeAll();
+                openHome();
+                currPage='home';
+                tit='HOME';
+                l=tit.length;
+            }
+            else {
+                const targetSection = document.querySelector(targetId);
+                targetSection.style.display = 'grid';
+                currPage=targetId;
+                tit=targetId.substring(1).toUpperCase();
+                l=tit.length;
+            }
+        });
+    });
 });
-
-
+ 
 document.addEventListener('DOMContentLoaded', function() {setTimeout(() => {
     homeLoad = setInterval(home, 2000);
     }, 1000);
     setTimeout(() => {
+        imgs.forEach(img => {
+            img.style.pointerEvents = 'auto';
+        });
         buttons.forEach(button => {
             button.style.pointerEvents = 'auto';
     });},1500)
 })
+
+setInterval(titlee,500)
